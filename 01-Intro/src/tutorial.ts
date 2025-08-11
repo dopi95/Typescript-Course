a6bc946a2a9ec3e36aa10bbdf0d304e95233c929
dopi95 <elyasyenealem@gmail.com>
2025-08-11 12:44:11 +0300

    Complete intro, type annotations, and arrays sections

---
+++ 01-Intro/src/tutorial.ts
@@ -0,0 +1,82 @@
+// let awesomeName:string = 'shakeAndBake';
+// awesomeName='something';
+// awesomeName= awesomeName.toUpperCase();
+// console.log(awesomeName);
+
+// // awesomeName =20;
+
+// let amount: number = 20;
+// amount = 12-1;
+
+// // amount ='pants'
+
+// let isAwesome: boolean = true;
+// isAwesome =false;
+
+// // isAwesome ='shakeAndBake'
+
+// //challenge
+// //1.
+// let  cvarstring: string = 'CHALLENGE';
+//      cvarstring =cvarstring.toLowerCase();
+// //2.
+// let cvarnum:number =30;
+//     cvarnum = cvarnum + 2;
+// //3.
+// let cvarboolean:boolean = cvarnum>=18;
+//     cvarboolean = !cvarboolean;
+
+//     console.log(cvarstring,cvarnum,cvarboolean);
+
+// let tax:number | string = 10 ;
+// tax =100;
+// tax = '$10';
+
+// let requestStatus: 'pending' | 'success' | 'error' = 'pending';
+// requestStatus = 'success';
+// requestStatus = 'error';
+
+// let notsure: any = 4;
+// notsure = 'david';
+// notsure = false;
+
+// const books = ['1984','Brave New World', 'Fahhrenheit 451'];
+
+// let foundBook: string | undefined;
+
+
+// for (let book of books){
+//     if(book === '1984'){
+//         foundBook = book;
+//         foundBook = foundBook.toUpperCase();
+//         break;
+//     }
+// }
+// console.log(foundBook?.length);
+
+// let discount: string | number = 20;
+// discount='20%'
+
+// let orderStatus: 'processing' | 'shipped' | 'delivered' = 'processing';
+// orderStatus ='shipped'
+
+//Arays
+let prices: number[] = [100,75,42];
+
+let fruit: string[] = ['apple', 'orange'];
+
+let emptyValue: number[] =[];
+
+let names = ['peter', 'susan', 1];
+let array: (string | boolean)[] = ['apple', true, 'orange', false];
+
+//Challanges
+let temperatures: number[] = [20,25,30];
+let color: string[] = ['red', 'green', 'yellow'];
+
+let mixedArray: (number | string)[] = [1, 'two',3];
+console.log(mixedArray);
+
+
+
+
